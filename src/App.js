f10d7fe875174bdd179ef2b8f05eaaf0144f8ea1
horseman7677 <horseman7677@gmail.com>
2022-04-05 15:16:23 +0530

    C Write

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./pages/Home";
 import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 
 // component ready 19:54
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
       <TopBar />
-      <Single />
+      <Write />
     </>
   );
 }
---
+++ src/pages/write/Write.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./write.css";
+
+function Write() {
+  return (
+    <div className="write">
+      <img
+        className="writeImg"
+        src="https://images.unsplash.com/photo-1639502003030-34c1d317e890?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80"
+        alt=""
+      />
+      <form className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+            <i className="writeIcon fas fa-plus"></i>
+          </label>
+          <input type="file" id="fileInput" style={{ display: "none" }} />
+          <input
+            type="text"
+            id="Title"
+            placeholder="Title"
+            className="writeInput"
+            autoFocus={true}
+          />
+        </div>
+        <div className="writeFormGroup">
+          <textarea
+            placeholder="Tell your story....."
+            type="text"
+            className="writeInput writeText"
+          ></textarea>
+        </div>
+        <button className="writeSubmit">Publish</button>
+      </form>
+    </div>
+  );
+}
+
+export default Write;
